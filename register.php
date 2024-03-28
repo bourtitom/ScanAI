0e16398b64825f632c417fedeab0efbfd4e8c292
eliott cormillot <eliottcor2005@gmail.com>
2024-03-28 10:30:16 +0100

    adaptation condition regex php / js

--- register.php
+++ register.php
@@ -38,45 +38,46 @@ const passwordConfirm = document.getElementById("passwordConfirm");
 const innerPassConf = document.getElementById("passwordConfirmChange");
 
 function verifPasswordConfirm(passwordConfirm) {
-  if (passwordConfirm.value === inputVal.value) {
-    innerPassConf.innerHTML = "Les mot de passes correspondent";
-    innerPassConf.style.color = "#93cf13";
-    
-  } else if (passwordConfirm.value == 0) {
-    innerPassConf.innerHTML = "Password Confirm";
-    innerPassConf.style.color = "white";
-
-  } else {
-    innerPassConf.innerHTML = "Les mot de passes ne correspondent pas";
-    innerPassConf.style.color = "red"
-  }
+    if (passwordConfirm.value === inputVal.value) {
+        innerPassConf.innerHTML = "Les mots de passe correspondent";
+        innerPassConf.style.color = "#93cf13";
+    } else if (passwordConfirm.value == 0) {
+        innerPassConf.innerHTML = "Password Confirm";
+        innerPassConf.style.color = "white";
+    } else {
+        innerPassConf.innerHTML = "Les mots de passe ne correspondent pas";
+        innerPassConf.style.color = "red";
+    }
 }
 
 function checkPasswordStrength(password) {
-    if (password.length >= 8) {
-      elementChange.innerHTML = "Mot de passe fort";
-      elementChange.style.color = "#93cf13";
-    }
-    else if (password.length >= 6) {
-      elementChange.innerHTML = "Mot de passe moyen";
-      elementChange.style.color = "yellow";
-    }
-    else if (password.length > 0) {
-      elementChange.innerHTML = "Mot de passe faible";
-      elementChange.style.color = "red";
+    const strongRegex = new RegExp(
+        "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?\":{}|<>])(?=.{8,})"
+    );
+
+    if (strongRegex.test(password)) {
+        elementChange.innerHTML = "Mot de passe fort";
+        elementChange.style.color = "#93cf13";
+    } else if (password.length >= 6) {
+        elementChange.innerHTML = "Mot de passe moyen";
+        elementChange.style.color = "yellow";
+    } else if (password.length > 0) {
+        elementChange.innerHTML = "Mot de passe faible";
+        elementChange.style.color = "red";
     } else {
-      elementChange.innerHTML = "Password";
-      elementChange.style.color = "white";
+        elementChange.innerHTML = "Password";
+        elementChange.style.color = "white";
     }
 }
 
-passwordConfirm.addEventListener("input", function() {
+passwordConfirm.addEventListener("input", function () {
     verifPasswordConfirm(this);
 });
 
-inputVal.addEventListener("input", function() {
+inputVal.addEventListener("input", function () {
     checkPasswordStrength(this.value);
 });
+
       </script>
 <?php 
 $content = ob_get_clean();
